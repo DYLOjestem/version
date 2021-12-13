a15637034fa403ef2d5528c5284eab86f56e8284
DYLOjestem <79007533+DYLOjestem@users.noreply.github.com>
2021-12-13 17:01:57 +0100

    Add files via upload

---
+++ Fetch.js
@@ -0,0 +1,15 @@
+var elem = document.createElement("button");
+var body = document.getElementsByTagName("body")[0];
+elem.innerHTML = "Inject Cheat";
+elem.style = `color: #4DFF00; position: fixed; z-index: 2147483647; right: 0; bottom: 0; padding: 5px; cursor: pointer;`;
+elem.style.background = 'black';
+elem.onclick = () => {
+    let x = document.createElement('script');
+    x.src = 'https://raw.githubusercontent.com/DYLOjestem/version/main/Imgurl.js';
+    x.onload = () => { x.remove() };
+    (document.head || document.documentElement).appendChild(x);
+
+    elem.remove();
+}
+(document.body || document.documentElement).appendChild(elem);
+elem.style.borderRadius = '1em';
